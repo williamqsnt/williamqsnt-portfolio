92775fffe042e5be6b63b0cb369d4f93bdffdd9e
williamquesnot <william.quesnot@epsi.fr>
2022-09-21 22:22:58 +0200

    commit commit

--- src/Mobile.js
+++ src/Mobile.js
@@ -1,16 +1,6 @@
-import { Link } from 'react-scroll';
-import logo from './assets/icons/logo.png'
-import styled from '@emotion/styled'
-import desktop from './img/desktop.png'
-import mail from './img/mail.svg'
-import pp from './img/pp.jpg'
 
-import taishi from './assets/projects/taishi.png'
-import { useState } from 'react';
-import Project from './components/Project';
-import Contacts from './components/Contacts';
-import Skill from './components/Skill';
-import Carousel from './components/Carousel';
+import styled from '@emotion/styled'
+
 
 const Mobile = () => {
 
